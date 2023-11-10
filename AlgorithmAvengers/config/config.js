0eada9ce69e74b7a01124ed166ad26ee8f303679
Vivian247 <144880443+Vivian247@users.noreply.github.com>
2023-11-10 18:38:50 -0500

    Database connected successfully

--- AlgorithmAvengers/config/config.js
+++ AlgorithmAvengers/config/config.js
@@ -1,5 +1,5 @@
 // Alert: For production environment never expose your connection string AND secret keys.
 module.exports = {
     "SECRETKEY": "Your_Secret_Key_Here",
-    "ATLASDB" : "mongodb+srv://appuser:1aMUhnGWpUZiE2XI@cluster06.c6o7sgg.mongodb.net/products?retryWrites=true&w=majority"
+    "ATLASDB" : "mongodb+srv://dbuser:fA3hb7Dd68yBhC0s@cluster0.hqx4l9d.mongodb.net/UsedBooksStore?retryWrites=true&w=majority"
 }
