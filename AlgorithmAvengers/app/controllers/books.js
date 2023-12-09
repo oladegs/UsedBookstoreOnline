ff0a9c4a4f4ebe6fbb7c8e94d46f26d345157ba9
ByeolAhn <bahn1@my.centennialcollege.ca>
2023-12-09 15:13:10 -0500

    add new field to controller/books.js create new function findBookByUserId

--- AlgorithmAvengers/app/controllers/books.js
+++ AlgorithmAvengers/app/controllers/books.js
@@ -3,7 +3,7 @@ let Book = require("../models/books");
 // Create a new book
 exports.create = async (req, res) => {
   try {
-    const { isbn, category, title, author, condition, price, description } =
+    const { isbn, category, title, author, condition, price, description, postedBy, expiryDate, active } =
       req.body;
     const newBook = new Book({
       isbn,
@@ -13,14 +13,16 @@ exports.create = async (req, res) => {
       condition,
       price,
       description,
-      postedBy: req.user._id,
+      postedBy,
+      expiryDate,
+      active
     });
 
     await newBook.save();
     res
       .status(201)
       .json({ message: "Book created successfully", book: newBook });
-      console.log({ isbn, category, title, author, condition, price, description, postedBy });
+      console.log({ isbn, category, title, author, condition, price, description, postedBy,expiryDate, active });
   } catch (error) {
     res
       .status(500)
@@ -44,10 +46,10 @@ exports.getAllBooks = async (req, res) => {
 exports.updateBook = async (req, res) => {
   try {
     const { isbn } = req.params;
-    const { category, title, author, condition, price, description } = req.body;
+    const { category, title, author, condition, price, description,expiryDate, active } = req.body;
     const updatedBook = await Book.findOneAndUpdate(
-      { isbn },
-      { category, title, author, condition, price, description },
+      { isbn, postedBy: req.user._id },
+      { category, title, author, condition, price, description, expiryDate, active },
       { new: true }
     );
 
@@ -56,7 +58,7 @@ exports.updateBook = async (req, res) => {
         .status(200)
         .json({ message: "Book updated successfully", book: updatedBook });
     } else {
-      res.status(404).json({ message: "Book not found" });
+      res.status(404).json({ message: "Book not found or unauthorized" });
     }
   } catch (error) {
     res
@@ -65,38 +67,21 @@ exports.updateBook = async (req, res) => {
   }
 };
 
-// Delete a book
-exports.deleteBook = async (req, res) => {
-  try {
-    const { isbn } = req.params;
-    const deletedBook = await Book.findOneAndDelete({ isbn });
 
-    if (deletedBook) {
-      res.status(200).json({ message: "Book deleted successfully" });
+// Find a book by user ID
+exports.findBookByUserId = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const books = await Book.find({ postedBy: userId });
+
+    if (books) {
+      res.status(200).json(books);
     } else {
-      res.status(404).json({ message: "Book not found" });
+      res.status(404).json({ message: "Books not found" });
     }
   } catch (error) {
     res
       .status(500)
-      .json({ message: "Error deleting book", error: error.message });
-  }
-};
-
-// Find a book by ISBN
-exports.findBookByISBN = async (req, res) => {
-  try {
-    const { isbn } = req.params;
-    const book = await Book.findOne({ isbn });
-
-    if (book) {
-      res.status(200).json(book);
-    } else {
-      res.status(404).json({ message: "Book not found" });
-    }
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error finding book", error: error.message });
+      .json({ message: "Error finding books", error: error.message });
   }
 };
